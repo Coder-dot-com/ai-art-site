61453cf6c7dbfcaaba955661dec6ee4ee9350bd3
usman17.s@hotmail.com <usman17.s@hotmail.com>
2022-09-21 13:42:02 +0100

    deploy

--- quiz_site/create_design/admin.py
+++ quiz_site/create_design/admin.py
@@ -4,6 +4,10 @@ from .models import BuyOptions, CreateDesignRequest, Effect
 
 # Register your models here.
 
+class BuyOptionAdmin(admin.ModelAdmin):
+    list_display = ['type_of_purchase', 'orientation', 'size', 'price', 'price_before_sale']
+
+
 admin.site.register(CreateDesignRequest)
 admin.site.register(Effect)
-admin.site.register(BuyOptions)
+admin.site.register(BuyOptions, BuyOptionAdmin)
--- quiz_site/create_design_buy/admin.py
+++ quiz_site/create_design_buy/admin.py
@@ -4,6 +4,5 @@ from .models import Order, ShippingOption
 
 # Register your models here.
 
-
 admin.site.register(ShippingOption)
 admin.site.register(Order)
